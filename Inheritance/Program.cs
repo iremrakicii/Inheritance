874abf80fd21e5376981eecce9d15c08e003fe77
iremrakicii <iremrakicii53@gmail.com>
2024-10-21 17:29:32 +0300

    Update Program.cs

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        ogretmen ogretmena = new ogretmen("Ayşe", "Öztürk", 30000);
+        Ogretmen ogretmena = new ogretmen("Ayşe", "Öztürk", 30000);
         ogretmena.YazdirOgretmenBilgileri();
 
         Console.WriteLine("-------------------------------------------------------");
 
-        ogrenci ogrenci = new ogrenci("İrem", "Rakıcı", 227);
+        Ogrenci ogrenci = new ogrenci("İrem", "Rakıcı", 227);
         ogrenci.YazdirOgrenciBilgileri();
 
     }
