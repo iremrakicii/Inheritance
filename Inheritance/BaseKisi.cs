12a3f72a9351f0ad01631ca3b12472a5ac903f4f
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-21 16:43:01 +0300

    Proje dosyası ekle.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,51 @@
+﻿namespace Inheritance
+{
+    public class BaseKisi
+    {
+        public string FirstName;
+        public string LastName;
+
+        public BaseKisi(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public void YazdirAdSoyad()
+        {
+            Console.WriteLine($"Ad: {FirstName}, Soyad: {LastName}");
+        }
+
+    }
+    public class ogrenci : BaseKisi
+    {
+        public int StudentNumber;
+
+        public ogrenci(string firstname, string lastname, int studentNumber) : base(firstname, lastname)
+        {
+            this.StudentNumber = studentNumber;
+        }
+        public void YazdirOgrenciBilgileri()
+        {
+            Console.WriteLine("Öğrencimizin bilgileri aşağıdaki gibidir:");
+            YazdirAdSoyad();
+            Console.WriteLine($"Öğrenci Numarası: {StudentNumber}");
+        }
+
+    }
+    public class ogretmen : BaseKisi
+    {
+        public decimal Pay;
+        public ogretmen(string firstname, string lastname, decimal pay) : base(firstname, lastname)
+        {
+
+            this.Pay = pay;
+        }
+        public void YazdirOgretmenBilgileri()
+        {
+            Console.WriteLine("Öğretmenimizin bilgileri aşağıdaki gibidir:");
+            YazdirAdSoyad();
+            Console.WriteLine($"Maaş bilgisi: {Pay}");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ogretmen ogretmena = new ogretmen("Ayşe", "Öztürk", 30000);
+        ogretmena.YazdirOgretmenBilgileri();
+
+        Console.WriteLine("-------------------------------------------------------");
+
+        ogrenci ogrenci = new ogrenci("İrem", "Rakıcı", 227);
+        ogrenci.YazdirOgrenciBilgileri();
+
+    }
+}
+
